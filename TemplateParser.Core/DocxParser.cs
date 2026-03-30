e78d18fc56107c60bf6c2141ee031d112beb1035
Kyle Sherbert <ksherbert@hcc-nd.edu>
2026-03-30 08:18:09 -0400

    Reconcile with NET 10.0

--- TemplateParser.Core/DocxParser.cs
+++ TemplateParser.Core/DocxParser.cs
@@ -1,5 +1,8 @@
 namespace TemplateParser.Core;
 
+using DocumentFormat.OpenXml.Packaging;         // Needed for WordProcessingDocument.
+using DocumentFormat.OpenXml.Wordprocessing;    // Needed for all Word schema objects (Body, Paragraph, etc.)
+
 public sealed class DocxParser
 {
     public ParserResult ParseDocxTemplate(string filePath, Guid templateId)
@@ -24,6 +27,39 @@ public sealed class DocxParser
         // - In Week 6, refactor large blocks from this method into focused helper classes.
         //
         // Do not place parsing logic in the CLI project; keep it in Core.
-        throw new NotImplementedException("DOCX parsing is intentionally not implemented in this starter repository.");
+        // throw new NotImplementedException("DOCX parsing is intentionally not implemented in this starter repository.");
+
+        
+        // 1. Open the word document in read mode.
+        // 2. Parse the document.xml into XML object using the DocumentFormat.OpenXml library.
+        using (WordprocessingDocument wordProcessingDocument = WordprocessingDocument.Open(filePath, false))
+        {
+            // The original line we wrote in class:
+            // Body body = wordProcessingDocument.MainDocumentPart.Document.Body;
+            
+            // A more robust version that fails gracefully if the document is not structured properly:
+            Body? body = wordProcessingDocument?.MainDocumentPart?.Document?.Body;
+            ArgumentNullException.ThrowIfNull(body, "Document is empty.");
+            
+            // 3. Loop through every paragraph.
+            foreach (Paragraph p in body.Descendants<Paragraph>())
+            {
+                // 4. Extract and display the paragraph style.
+                // The original line we wrote in class:
+                // string style = p?.ParagraphProperties?.ParagraphStyleId?.Val;
+                // A more robust version:
+                string? style = p?.ParagraphProperties?.ParagraphStyleId?.Val ?? "No Style";
+                Console.WriteLine(style);
+
+                // 5. Extract and display the actual text.
+                string? text = p?.InnerText;
+                Console.WriteLine(text);
+
+                // I added the following line to space the output out a little better:
+                Console.WriteLine("--------------------------------");
+            }
+        }
+
+        return null;
     }
 }
